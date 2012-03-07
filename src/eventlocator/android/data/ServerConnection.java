2317f33530f962495d75b9594874dcddfaf9eb57
Michael Harris <michaelharris@gmx.com>
2012-03-07 22:40:18 +0000

    fixed wrong package name in Manifest. Added refresh events button.

--- src/eventlocator/android/data/ServerConnection.java
+++ src/eventlocator/android/data/ServerConnection.java
@@ -1,36 +1,31 @@
 package eventlocator.android.data;
 
-
 import java.util.ArrayList;
-
-import eventlocator.android.R;
-
-
 
 public class ServerConnection {
 	ArrayList<Event> events;
-	
-	public ServerConnection(String url){
+/**
+ * The class used by the activity to get the events
+ * @param url
+ */
+	public ServerConnection(String url) {
 		String jsonFromServer;
 		events = new ArrayList<Event>();
 		try {
-		
 			jsonFromServer = JSONClient.connect(url);
-			JSONToObjects  jsonToObjects = new JSONToObjects();
+			JSONToObjects jsonToObjects = new JSONToObjects();
 			jsonToObjects.init(jsonFromServer);
 			events = jsonToObjects.findAll();
-			
+
 		} catch (Exception e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		
-		
+
 	}
+
 	public ArrayList<Event> getEvents() {
 		return events;
 	}
-	
 
 }
