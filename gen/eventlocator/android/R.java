487fb6aaeeb11ed2752277e4213d5dd4b4910892
Michael Harris <michaelharris@gmx.com>
2012-04-27 17:10:59 +0100

    Text in event dialog now scrolls. Event dialog also shows advert images.

--- gen/eventlocator/android/R.java
+++ gen/eventlocator/android/R.java
@@ -11,19 +11,25 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_event_pin=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int ic_menu_mylocation=0x7f020002;
-        public static final int ic_menu_refresh=0x7f020003;
-        public static final int iconold=0x7f020004;
+        public static final int advert_msc=0x7f020000;
+        public static final int advert_phd=0x7f020001;
+        public static final int ic_event_pin=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int ic_menu_mylocation=0x7f020004;
+        public static final int ic_menu_refresh=0x7f020005;
+        public static final int iconold=0x7f020006;
     }
     public static final class id {
-        public static final int centre_loc=0x7f070001;
-        public static final int mapview=0x7f070000;
-        public static final int refresh_events=0x7f070002;
+        public static final int centre_loc=0x7f070004;
+        public static final int image=0x7f070001;
+        public static final int layout_root=0x7f070000;
+        public static final int mapview=0x7f070003;
+        public static final int refresh_events=0x7f070005;
+        public static final int text=0x7f070002;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int event_dialog=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
         public static final int map_menu=0x7f060000;
--- src/eventlocator/android/EventItemizedOverlay.java
+++ src/eventlocator/android/EventItemizedOverlay.java
@@ -1,50 +1,64 @@
 package eventlocator.android;
+
 import java.util.ArrayList;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.OverlayItem;
 
 public class EventItemizedOverlay extends ItemizedOverlay {
 	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
-	 Context mContext;
-	 	 
+	Context mContext;
+
 	public EventItemizedOverlay(Drawable defaultMarker) {
-		 super(boundCenterBottom(defaultMarker));
-		 populate();
-		
+		super(boundCenterBottom(defaultMarker));
+		populate();
+
 	}
-	
+
 	public EventItemizedOverlay(Drawable defaultMarker, Context context) {
-		  super(boundCenterBottom(defaultMarker));
-		  mContext = context;
-		  populate();
-		}
+		super(boundCenterBottom(defaultMarker));
+		mContext = context;
+		populate();
+	}
+
 	@Override
 	protected OverlayItem createItem(int i) {
 		return mOverlays.get(i);
 	}
+
 	@Override
 	public int size() {
-		 return mOverlays.size();
+		return mOverlays.size();
 	}
-	
+
 	public void addOverlay(OverlayItem overlay) {
-	    mOverlays.add(overlay);
-	    populate();
+		mOverlays.add(overlay);
+		populate();
 	}
-	
+
 	@Override
 	protected boolean onTap(int index) {
-	  OverlayItem item = mOverlays.get(index);
-	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
-	  dialog.setTitle(item.getTitle());
-	  dialog.setMessage(item.getSnippet());
-	  dialog.show();
-	  return true;
+		OverlayItem item = mOverlays.get(index);
+		Dialog dialog = new Dialog(mContext);
+
+		dialog.setContentView(R.layout.event_dialog);
+		dialog.setTitle(item.getTitle());
+
+		TextView text = (TextView) dialog.findViewById(R.id.text);
+		text.setText(item.getSnippet() + item.getSnippet() + item.getSnippet());
+		text.setMovementMethod(new ScrollingMovementMethod());
+		ImageView image = (ImageView) dialog.findViewById(R.id.image);
+		image.setImageResource(R.drawable.advert_phd);
+		dialog.show();
+		return true;
 	}
 
 }
